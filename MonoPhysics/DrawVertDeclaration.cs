2f07fcbc035fafd0a6a14987e77b332527c5bc7c
Sebastian Meier zu Biesen <mitoskalandiel@gmail.com>
2025-03-16 23:05:32 +0000

    Add ImGui integration and update project dependencies
    Updated MonoPhysics.Core.Tests.csproj:
    - Upgraded coverlet.collector to 6.0.4 with PrivateAssets and IncludeAssets.
    - Added Microsoft.NET.Test.Sdk 17.13.0.
    - Added MSTest.Analyzers 3.8.2 with PrivateAssets and IncludeAssets.
    - Added MSTest.TestAdapter 3.8.2.
    - Added MSTest.TestFramework 3.8.2.
    
    Updated MonoPhysics.csproj:
    - Replaced MonoGame.Framework.DesktopGL 3.8.1.303 with 3.8.2.1105.
    - Replaced MonoGame.Content.Builder.Task 3.8.1.303 with 3.8.2.1105.
    - Added ImGui.NET 1.91.6.1.
    
    Modified Physics.cs:
    - Added _imGuiRenderer field and initialized it in the constructor.
    - Added ImGui rendering calls in the Draw method.
    
    Updated README.md:
    - Added build status badges.
    - Added sections for aim, current state, and libraries used.
    
    Added ImGuiRenderer.cs:
    - Implemented ImGuiRenderer class for integrating ImGui with MonoGame.
    
    Integrate ImGui.NET and update project dependencies
    
    Updated MonoPhysics.Core.Tests.csproj:
    - Upgraded coverlet.collector to 6.0.4 and added PrivateAssets and IncludeAssets attributes.
    - Added Microsoft.NET.Test.Sdk 17.13.0.
    - Added MSTest.Analyzers 3.8.2 with PrivateAssets and IncludeAssets attributes.
    - Added MSTest.TestAdapter 3.8.2.
    - Added MSTest.TestFramework 3.8.2.
    
    Updated MonoPhysics.csproj:
    - Added <AllowUnsafeBlocks>True</AllowUnsafeBlocks> property.
    - Replaced MonoGame.Framework.DesktopGL and MonoGame.Content.Builder.Task with version 3.8.2.1105.
    - Added ImGui.NET 1.91.6.1.
    
    Updated Physics.cs:
    - Added ImGui.NET and System.Numerics namespaces.
    - Added fields for ImGui integration.
    - Added methods for ImGui integration and modified existing methods to incorporate ImGui rendering.
    
    Updated README.md:
    - Added build status badges.
    - Updated aim and current state of the software.
    - Listed libraries used.
    
    Added DrawVertDeclaration.cs:
    - Defined DrawVertDeclaration class with vertex declaration and size.
    
    Added ImGuiRenderer.cs:
    - Defined ImGuiRenderer class for handling ImGui rendering within MonoGame.
    - Implemented methods for input setup, buffer updates, and rendering ImGui draw data.
    
    These changes introduce ImGui.NET integration, enhancing the UI rendering capabilities within the MonoPhysics project.

---
+++ MonoPhysics/DrawVertDeclaration.cs
@@ -0,0 +1,55 @@
+﻿using ImGuiNET;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoPhysics
+{
+    /// <summary>
+    /// DrawVertDeclaration class
+    /// </summary>
+    public static class DrawVertDeclaration
+    {
+        #region Fields
+
+        /// <summary>
+        /// The declaration of a vertex
+        /// </summary>
+        public static readonly VertexDeclaration Declaration;
+
+        /// <summary>
+        /// The size of the vertex
+        /// </summary>
+        public static readonly int Size;
+
+        #endregion Fields
+
+        #region Public Constructors
+
+        /// <summary>
+        /// Static constructor
+        /// </summary>
+        static DrawVertDeclaration()
+        {
+            unsafe { Size = sizeof(ImDrawVert); }
+
+            Declaration = new VertexDeclaration(
+                Size,
+
+                // Position
+                new VertexElement(0, VertexElementFormat.Vector2, VertexElementUsage.Position, 0),
+
+                // UV
+                new VertexElement(8, VertexElementFormat.Vector2, VertexElementUsage.TextureCoordinate, 0),
+
+                // Color
+                new VertexElement(16, VertexElementFormat.Color, VertexElementUsage.Color, 0)
+            );
+        }
+
+        #endregion Public Constructors
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # MonoPhysics
 
-[![.NET](https://github.com/TheNoviceProspect/MonoPhysics/actions/workflows/dotnet.yml/badge.svg)](https://github.com/TheNoviceProspect/MonoPhysics/actions/workflows/dotnet.yml)
+### Build Status
+[![.NET build](https://github.com/TheNoviceProspect/MonoPhysics/actions/workflows/build.yml/badge.svg)](https://github.com/TheNoviceProspect/MonoPhysics/actions/workflows/build.yml)
+
+[![.NET Tests](https://github.com/TheNoviceProspect/MonoPhysics/actions/workflows/test.yml/badge.svg)](https://github.com/TheNoviceProspect/MonoPhysics/actions/workflows/test.yml)
+
+[![.NET Deploy](https://github.com/TheNoviceProspect/MonoPhysics/actions/workflows/deploy.yml/badge.svg)](https://github.com/TheNoviceProspect/MonoPhysics/actions/workflows/deploy.yml)
+
+## The Aim of this software
+
+In the long run I want to be able to visualise the physics of a system. (or multiples thereof)
+
+## Current State
+
+Currently I am working on a simple physics engine and making things on the screen appear
+
+## Libraries used
+
+- [MonoGame](https://www.monogame.net/)
+- [Box2D](https://box2d.org/)
+- [.Net 9](https://dotnet.microsoft.com/download/dotnet/9.0)]
+- [ImGui.NET](https://github.com/ImGuiNET/ImGui.NET)
